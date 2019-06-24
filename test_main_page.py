26d49480a11594b961e8b7c5a960835c97be0e4b
ttwebm <ttwebm@gmail.com>
2019-06-24 21:23:39 +0300

    add new link testing, catch exception and correcting log message

--- test_main_page.py
+++ test_main_page.py
@@ -10,4 +10,10 @@ def test_guest_can_go_to_login_page(browser):
     page.open()
     # выполняем метод страницы - переходим на страницу логина
     page.go_to_login_page()
-    time.sleep(30)
+    # time.sleep(30)
+
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
