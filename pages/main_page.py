5abb432eff50ab82055b2c14537b1cc6546cb762
ttwebm <ttwebm@gmail.com>
2019-06-27 20:18:52 +0300

    add redirection functional

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,12 +1,11 @@
-from selenium.webdriver.common.by import By
+from .locators import MainPageLocators as MPL
 from .base_page import BasePage
 
 
 class MainPage(BasePage):
     def go_to_login_page(self):
-        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        login_link = self.browser.find_element(*MPL.LOGIN_LINK)
         login_link.click()
 
     def should_be_login_link(self):
-        assert self.is_element_present(By.CSS_SELECTOR,
-            "#login_link"), "Login link is not presented"
+        assert self.is_element_present(*MPL.LOGIN_LINK), "Login link is not presented"
--- test_main_page.py
+++ test_main_page.py
@@ -3,7 +3,7 @@ from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
 
 
-def ttest_guest_can_go_to_login_page(browser):
+def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     # инициализируем Page Object, передаем
     # в конструктор экземпляр драйвера и url адрес
@@ -12,16 +12,12 @@ def ttest_guest_can_go_to_login_page(browser):
     page.open()
     # выполняем метод страницы - переходим на страницу логина
     page.go_to_login_page()
-    # time.sleep(30)
+    # time.sleep(10)
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
 
-def ttest_guest_should_see_login_link(browser):
+def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
-
-def test_guest_should_see_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
-    page = LoginPage(browser, link)
-    page.open()
-    page.should_be_login_page()
